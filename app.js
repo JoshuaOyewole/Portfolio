d8da5aab7c4ad286a85aac4c4b3d0ab3336a5655
Joshua Oyewole <joshpp2013@gmail.com>
2020-08-19 23:33:20 +0100

    Build complete

---
+++ app.js
@@ -0,0 +1,10 @@
+//DYNAMIC FOOTER YEAR
+//document.getElementById('date').innerText = new Date().getFullYear();
+
+//RESPONSIVE MENU
+const toggleBtn = document.getElementById('hidden');
+const nav =  document.querySelector('.list-group');
+toggleBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('block');
+    nav.classList.toggle('show');
+});
